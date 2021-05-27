9444355b4175a052c316e552eb5fef0836e21070
embedded-explorer <n.hrishikesh.prabhu@gmail.com>
2021-05-27 18:14:42 +0530

    Adding multiple_modules

---
+++ multiple_modules/multiple_modules.v
@@ -0,0 +1,14 @@
+module sub_module2 (input a, input b, output y);
+	assign y = a | b;
+endmodule
+
+module sub_module1 (input a, input b, output y);
+	assign y = a&b;
+endmodule
+
+
+module multiple_modules (input a, input b, input c , output y);
+	wire net1;
+	sub_module1 u1(.a(a),.b(b),.y(net1));  //net1 = a&b
+	sub_module2 u2(.a(net1),.b(c),.y(y));  //y = net1|c ,ie y = a&b + c;
+endmodule
