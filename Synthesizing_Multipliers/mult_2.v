1865500e8c1af89ef671ca489ad42683e3f9ba67
embedded-explorer <n.hrishikesh.prabhu@gmail.com>
2021-06-06 11:27:11 +0530

    deleting old files

--- Synthesizing_Multipliers/mult_2.v
+++
@@ -1,6 +0,0 @@
-module mult_2 (
-	input 	[2:0] 	a, 
-	output 	[3:0] 	y
-);
-	assign y = a * 2;
-endmodule
--- Synthesizing_Multipliers/mult_9.v
+++
@@ -1,7 +0,0 @@
-module mult_9 (
-	input 	[2:0] 	a , 
-	output 	[5:0] 	y
-);
-	assign y = a * 9;
-endmodule
-
